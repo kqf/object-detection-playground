2fe14ed72aa1c5b90f0338fa120548e279a91396
kqf <q.dev.null@gmail.com>
2021-03-03 16:36:54 +0100

    Add the tests for the old dataset (#37)
    * Add the tests for the old dataset
    
    * Clean the DetectionDataset from dicom dependencies

---
+++ tests/test_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from detection.data import DetectionDataset
+
+
+def test_dataset(fake_dataset):
+    df = pd.read_csv(fake_dataset / "train.csv")
+    dataset = DetectionDataset(df, fake_dataset)
+
+    for image, targets in dataset:
+        assert len(image.shape) == 3, "There are only 3 dimensions"
+        assert image.shape[-1] == 3, "There are only 3 channels"
--- tests/test_mc.py
+++ tests/test_mc.py
@@ -4,6 +4,5 @@ from detection.mc import make_blob, blob2image
 
 def test_mc():
     output = blob2image(make_blob())
-    print(output)
     plt.imshow(output)
     plt.show()
