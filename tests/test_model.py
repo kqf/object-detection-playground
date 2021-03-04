0b1be739ba38501bf54a83349fd097cdc137f712
Sasha <q.dev.null@gmail.com>
2021-03-04 03:02:50 +0100

    Add the testing module

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,12 @@
 import pandas as pd
+from detection.data import DetectionDataset
+from detection.model import build_model
 
 
 def test_dummy(fake_dataset):
     df = pd.read_csv(fake_dataset / "train.csv")
     print(df.head())
+    train = DetectionDataset(df, fake_dataset)
+
+    model = build_model()
+    model.fit(train)
