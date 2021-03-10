ecad271da62a8d02704040c30153ae4333c0a77e
kqf <q.dev.null@gmail.com>
2021-03-10 21:20:31 +0100

    Fix the network (#48)

--- detection/darknet.py
+++ detection/darknet.py
@@ -19,17 +19,20 @@ class ResidualBlock(torch.nn.Module):
     def __init__(self, channels, num_repeats=1):
         super().__init__()
         self.layer = torch.nn.Sequential(
-            conv(channels, channels // 2, kernel_size=1),
-            conv(channels // 2, channels, kernel_size=3, padding=1),
+            conv(channels, channels // 2, kernel_size=1, padding=0),
+            conv(channels // 2, channels, kernel_size=3),
         )
 
     def forward(self, x):
         return x + self.layer(x)
 
 
-def conv(in_channels, out_channels, **kwargs):
+def conv(in_channels, out_channels, padding=1, **kwargs):
     block = torch.nn.Sequential(
-        torch.nn.Conv2d(in_channels, out_channels, bias=False, **kwargs),
+        torch.nn.Conv2d(
+            in_channels, out_channels,
+            bias=False, padding=padding, **kwargs
+        ),
         torch.nn.BatchNorm2d(out_channels),
         torch.nn.LeakyReLU(0.1),
     )
