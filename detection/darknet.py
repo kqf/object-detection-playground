e5679d905c2e03c0a94e6a390a2afa7f711bb6f2
kqf <q.dev.null@gmail.com>
2021-03-10 21:23:45 +0100

    Simplify the darknet (#49)
    * Remove the dead code
    
    * Fix the unused variables
    
    * Remove the configuration

--- detection/darknet.py
+++ detection/darknet.py
@@ -1,19 +1,5 @@
 import torch
 
-config = [
-    (32, 3, 1),
-    (64, 3, 2),
-    [1],
-    (128, 3, 2),
-    [2],
-    (256, 3, 2),
-    [8],
-    (512, 3, 2),
-    [8],
-    (1024, 3, 2),
-    [4],
-]
-
 
 class ResidualBlock(torch.nn.Module):
     def __init__(self, channels, num_repeats=1):
@@ -47,26 +33,8 @@ def residual(channels, num_repeats):
 
 
 def build_darknet(in_channels=3):
-    layers = []
-    for module in config:
-        if isinstance(module, tuple):
-            out_channels, kernel_size, stride = module
-            layers.append(
-                conv(
-                    in_channels,
-                    out_channels,
-                    kernel_size=kernel_size,
-                    stride=stride,
-                    padding=1 if kernel_size == 3 else 0,
-                )
-            )
-            in_channels = out_channels
-
-        if isinstance(module, list):
-            layers.append(residual(in_channels, num_repeats=module[0],))
-
     model = torch.nn.Sequential(
-        conv(3, 32, kernel_size=3, stride=1),
+        conv(in_channels, 32, kernel_size=3, stride=1),
         conv(32, 64, kernel_size=3, stride=2),
         residual(64, num_repeats=1),
         conv(64, 128, kernel_size=3, stride=2),
