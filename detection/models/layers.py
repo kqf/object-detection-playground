90155a759ba719900d0f9c4b368ea1c7df8a1ba8
Sasha <q.dev.null@gmail.com>
2021-03-11 20:32:57 +0100

    Move the fasst RCNN to a separate module

--- detection/models/layers.py
+++ detection/models/layers.py
@@ -4,31 +4,6 @@ import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 
-class FasterRCNN(torch.nn.Module):
-    def __init__(self, n_classes=15, pretrained=False):
-        super().__init__()
-        self.backbone = torchvision.models.detection.fasterrcnn_resnet50_fpn(
-            pretrained=pretrained)
-
-        in_features = (
-            self.backbone
-            .roi_heads
-            .box_predictor
-            .cls_score.in_features
-        )
-        self.backbone.roi_heads.box_predictor = FastRCNNPredictor(
-            in_features, n_classes)
-
-    def forward(self, x, targets=None):
-        if not self.train:
-            return self.backbone(x)
-
-        if self.train and targets is None:
-            return x
-
-        losses = self.backbone(x, targets)
-        return sum(loss for loss in losses.values())
-
 
 class YOLO(torch.nn.Module):
     def __init__(self, n_classes=15, pretrained=False):
