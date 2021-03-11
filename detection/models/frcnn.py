da8b54db71bdb8e8b8c478dd4a053b7e006aa5ce
kqf <q.dev.null@gmail.com>
2021-03-11 20:36:51 +0100

    Separate the detection models (#51)
    * Move the fasst RCNN to a separate module
    
    * Fix the unused imports
    
    * Separate the yolo v5 model

--- detection/models/frcnn.py
+++ detection/models/frcnn.py
@@ -28,20 +28,3 @@ class FasterRCNN(torch.nn.Module):
 
         losses = self.backbone(x, targets)
         return sum(loss for loss in losses.values())
-
-
-class YOLO(torch.nn.Module):
-    def __init__(self, n_classes=15, pretrained=False):
-        super().__init__()
-        self.model = torch.hub.load(
-            'ultralytics/yolov5', 'yolov5s',
-            pretrained=pretrained, autoshape=False)
-
-    def forward(self, x):
-        return self.model(x)
-
-
-class Loss(torch.nn.Module):
-    def forward(self, y_pred, y_true):
-        small, medium, large = y_pred
-        return large.sum()
