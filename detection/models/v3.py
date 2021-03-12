eebd8bdc2f933b1eeda0c7dd82f32b38f7f386f1
Sasha <q.dev.null@gmail.com>
2021-03-12 18:39:42 +0100

    Remove the building function

--- detection/models/v3.py
+++ detection/models/v3.py
@@ -1,29 +1,15 @@
 import torch
 import torch.nn as nn
 
-from detection.models.darknet import conv
-
-config = [
-    (512, 1, 1),
-    (1024, 3, 1),
-    "S",
-    (256, 1, 1),
-    "U",
-    (256, 1, 1),
-    (512, 3, 1),
-    "S",
-    (128, 1, 1),
-    "U",
-    (128, 1, 1),
-    (256, 3, 1),
-    "S",
-]
+from detection.models.darknet import conv, block
 
 
 class ScalePrediction(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
         self.pred = nn.Sequential(
+            block(2 * in_channels),
+            conv(2 * in_channels, in_channels, kernel_size=1),
             conv(in_channels, 2 * in_channels, kernel_size=3, padding=1),
             torch.nn.Conv2d(
                 2 * in_channels, (num_classes + 5) * 3, kernel_size=1
@@ -48,6 +34,23 @@ class YOLOv3(nn.Module):
         self.in_channels = in_channels
         self.layers = self._create_conv_layers()
 
+        self.layers = torch.nn.Sequential(
+            conv(1024, 512, 1, 1),
+            conv(512, 1024, 3, 1),
+            ScalePrediction(1024 // 2, num_classes),
+            conv(1024 // 2, 256, 1, 1),
+            torch.nn.Upsample(scale_factor=2),
+            conv(256, 256, 1, 1),
+            conv(256, 512, 3, 1),
+            ScalePrediction(512 // 2, num_classes),
+            conv(256, 128, 1, 1),
+            torch.nn.Upsample(scale_factor=2),
+            conv(128, 128, 1, 1),
+            conv(128, 256, 3, 1),
+            ScalePrediction(256 // 2, num_classes),
+
+        )
+
     def forward(self, x):
         outputs = []  # for each scale
         route_connections = []
@@ -58,7 +61,7 @@ class YOLOv3(nn.Module):
 
             x = layer(x)
 
-            if isinstance(layer, ResidualBlock) and layer.num_repeats == 8:
+            if isinstance(layer, block) and layer.num_repeats == 8:
                 route_connections.append(x)
 
             elif isinstance(layer, nn.Upsample):
@@ -66,25 +69,3 @@ class YOLOv3(nn.Module):
                 route_connections.pop()
 
         return outputs
-
-    def _create_conv_layers(self):
-        layers = nn.ModuleList()
-        in_channels = self.in_channels
-
-        for module in config:
-            if isinstance(module, str):
-                if module == "S":
-                    layers += [
-                        ResidualBlock(
-                            in_channels, use_residual=False, num_repeats=1),
-                        conv(in_channels, in_channels // 2, kernel_size=1),
-                        ScalePrediction(in_channels // 2,
-                                        num_classes=self.num_classes),
-                    ]
-                    in_channels = in_channels // 2
-
-                elif module == "U":
-                    layers.append(nn.Upsample(scale_factor=2),)
-                    in_channels = in_channels * 3
-
-        return layers
