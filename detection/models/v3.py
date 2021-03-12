8eedc9b58624368486bb6c1df834c1e57cc280ae
Sasha <q.dev.null@gmail.com>
2021-03-12 18:27:33 +0100

    Remove the redundant layers

--- detection/models/v3.py
+++ detection/models/v3.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from detection.models.darknet import conv
+
 config = [
     (512, 1, 1),
     (1024, 3, 1),
@@ -18,52 +20,11 @@ config = [
 ]
 
 
-class CNNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, bn_act=True, **kwargs):
-        super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels,
-                              bias=not bn_act, **kwargs)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.leaky = nn.LeakyReLU(0.1)
-        self.use_bn_act = bn_act
-
-    def forward(self, x):
-        if self.use_bn_act:
-            return self.leaky(self.bn(self.conv(x)))
-        return self.conv(x)
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, channels, use_residual=True, num_repeats=1):
-        super().__init__()
-        self.layers = nn.ModuleList()
-        for repeat in range(num_repeats):
-            self.layers += [
-                nn.Sequential(
-                    CNNBlock(channels, channels // 2, kernel_size=1),
-                    CNNBlock(channels // 2, channels,
-                             kernel_size=3, padding=1),
-                )
-            ]
-
-        self.use_residual = use_residual
-        self.num_repeats = num_repeats
-
-    def forward(self, x):
-        for layer in self.layers:
-            if self.use_residual:
-                x = x + layer(x)
-            else:
-                x = layer(x)
-
-        return x
-
-
 class ScalePrediction(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
         self.pred = nn.Sequential(
-            CNNBlock(in_channels, 2 * in_channels, kernel_size=3, padding=1),
+            conv(in_channels, 2 * in_channels, kernel_size=3, padding=1),
             torch.nn.Conv2d(
                 2 * in_channels, (num_classes + 5) * 3, kernel_size=1
             ),
@@ -111,30 +72,12 @@ class YOLOv3(nn.Module):
         in_channels = self.in_channels
 
         for module in config:
-            if isinstance(module, tuple):
-                out_channels, kernel_size, stride = module
-                layers.append(
-                    CNNBlock(
-                        in_channels,
-                        out_channels,
-                        kernel_size=kernel_size,
-                        stride=stride,
-                        padding=1 if kernel_size == 3 else 0,
-                    )
-                )
-                in_channels = out_channels
-
-            elif isinstance(module, list):
-                num_repeats = module[1]
-                layers.append(ResidualBlock(
-                    in_channels, num_repeats=num_repeats,))
-
-            elif isinstance(module, str):
+            if isinstance(module, str):
                 if module == "S":
                     layers += [
                         ResidualBlock(
                             in_channels, use_residual=False, num_repeats=1),
-                        CNNBlock(in_channels, in_channels // 2, kernel_size=1),
+                        conv(in_channels, in_channels // 2, kernel_size=1),
                         ScalePrediction(in_channels // 2,
                                         num_classes=self.num_classes),
                     ]
