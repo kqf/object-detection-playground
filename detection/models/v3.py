288074c74a35947de3fba69f2392832c89985b94
Sasha <q.dev.null@gmail.com>
2021-03-12 18:17:34 +0100

    Clean the linting issues

--- detection/models/v3.py
+++ detection/models/v3.py
@@ -65,8 +65,8 @@ class ScalePrediction(nn.Module):
         super().__init__()
         self.pred = nn.Sequential(
             CNNBlock(in_channels, 2 * in_channels, kernel_size=3, padding=1),
-            CNNBlock(
-                2 * in_channels, (num_classes + 5) * 3, bn_act=False, kernel_size=1
+            torch.nn.Conv2d(
+                2 * in_channels, (num_classes + 5) * 3, kernel_size=1
             ),
         )
         self.num_classes = num_classes
@@ -74,7 +74,9 @@ class ScalePrediction(nn.Module):
     def forward(self, x):
         return (
             self.pred(x)
-            .reshape(x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3])
+            .reshape(
+                x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3]
+            )
             .permute(0, 1, 3, 4, 2)
         )
 
