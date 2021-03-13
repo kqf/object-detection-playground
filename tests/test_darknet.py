bafb8be86f3f4d8a4a7a614b5b3fe5ac075f546c
Sasha <q.dev.null@gmail.com>
2021-03-13 13:34:19 +0100

    Add more tests

--- tests/test_darknet.py
+++ tests/test_darknet.py
@@ -11,4 +11,8 @@ def batch():
 
 def test_module(batch):
     model = Darknet()
-    assert model(batch).shape == (64, 1024, 8, 8)
+    l1, l2, l3 = model(batch)
+
+    assert l1.shape == (64, 1024, 8, 8)
+    assert l2.shape == (64, 512, 16, 16)
+    assert l3.shape == (64, 256, 32, 32)
