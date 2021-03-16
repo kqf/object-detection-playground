114e360710c56032e87d7a8945cb9c532203754b
kqf <q.dev.null@gmail.com>
2021-03-16 19:44:32 +0100

    Reduce the number of layers (#64)
    * Introduce the conv layers
    
    * Fix the model
    
    * Reorganize the blocks
    
    * Refactor the convolutional blocks

--- detection/models/v3.py
+++ detection/models/v3.py
@@ -4,13 +4,12 @@ import torch.nn as nn
 from detection.models.darknet import conv, block, Darknet
 
 
-def scaling(in_channels):
+def cblock(in_channels, out_channels):
     model = nn.Sequential(
-        conv(in_channels, in_channels // 2, kernel_size=1, stride=1),
-        conv(in_channels // 2, in_channels, kernel_size=3, stride=1,
-             padding=0),
+        conv(in_channels, out_channels, kernel_size=1, stride=1),
+        conv(out_channels, in_channels, kernel_size=3, stride=1, padding=0),
         block(in_channels),
-        conv(in_channels, in_channels // 2, kernel_size=1, padding=0),
+        conv(in_channels, out_channels, kernel_size=1, padding=0),
     )
     return model
 
@@ -18,7 +17,6 @@ def scaling(in_channels):
 class ScalePrediction(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
-        self.xscale = scaling(in_channels)
         self.pred = nn.Sequential(
             conv(in_channels // 2, in_channels, kernel_size=3, padding=1),
             torch.nn.Conv2d(in_channels, (num_classes + 5) * 3, kernel_size=1),
@@ -26,9 +24,8 @@ class ScalePrediction(nn.Module):
         self.num_classes = num_classes
 
     def forward(self, x):
-        xscale = self.xscale(x)
-        out = self.pred(xscale)
-        return xscale, (
+        out = self.pred(x)
+        return (
             out.reshape(
                 x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3]
             )
@@ -43,39 +40,43 @@ class YOLO(nn.Module):
         self.num_classes = num_classes
         self.in_channels = in_channels
 
-        self.scale1 = torch.nn.Sequential(
-            ScalePrediction(1024, num_classes),
+        self.conv1 = torch.nn.Sequential(
+            cblock(1024, 1024 // 2),
         )
 
         self.upsample2 = torch.nn.Sequential(
             conv(1024 // 2, 256, kernel_size=1, stride=1, padding=0),
             torch.nn.Upsample(scale_factor=2),
         )
-        self.scale2 = torch.nn.Sequential(
+        self.conv2 = torch.nn.Sequential(
             conv(256 * 3, 256, kernel_size=1, stride=1, padding=0),
             conv(256, 512, kernel_size=3, stride=1, padding=1),
-            ScalePrediction(512, num_classes),
+            cblock(512, 512 // 2),
         )
 
         self.upsample3 = torch.nn.Sequential(
             conv(256, 128, kernel_size=1, stride=1, padding=0),
             torch.nn.Upsample(scale_factor=2),
         )
-        self.scale3 = torch.nn.Sequential(
+        self.conv3 = torch.nn.Sequential(
             conv(128 * 3, 128, kernel_size=1, stride=1, padding=0),
             conv(128, 256, kernel_size=3, stride=1, padding=1),
-            ScalePrediction(256, num_classes),
-
+            cblock(256, 256 // 2),
         )
+
+        self.scale1 = ScalePrediction(1024, num_classes)
+        self.scale2 = ScalePrediction(512, num_classes)
+        self.scale3 = ScalePrediction(256, num_classes)
 
     def forward(self, x):
         l1, l2, l3 = self.backbone(x)
 
-        xscale, scale1 = self.scale1(l1)
-        x = self.upsample2(xscale)
-        xscale, scale2 = self.scale2(torch.cat([x, l2], dim=1))
+        x1 = self.conv1(l1)
 
-        x = self.upsample3(xscale)
-        _, scale3 = self.scale3(torch.cat([x, l3], dim=1))
+        xu = self.upsample2(x1)
+        x2 = self.conv2(torch.cat([xu, l2], dim=1))
 
-        return scale1, scale2, scale3
+        xu = self.upsample3(x2)
+        x3 = self.conv3(torch.cat([xu, l3], dim=1))
+
+        return self.scale1(x1), self.scale2(x2), self.scale3(x3)
