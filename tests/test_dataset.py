400a08ea7ebe4335dee8d8208a97b4c02faf8e44
Sasha <q.dev.null@gmail.com>
2021-03-17 21:51:35 +0100

    Fix the dataset sizes

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -10,6 +10,6 @@ def test_dataset(fake_dataset):
         assert len(image.shape) == 3, "There are only 3 dimensions"
         assert image.shape[-1] == 3, "There are only 3 channels"
 
-        assert s1.shape == (16, 3, 8, 8, 85)
-        assert s2.shape == (16, 3, 16, 16, 85)
-        assert s3.shape == (16, 3, 32, 32, 85)
+        assert s1.shape == (2, 13, 13, 6)
+        assert s2.shape == (2, 26, 26, 6)
+        assert s3.shape == (2, 52, 52, 6)
