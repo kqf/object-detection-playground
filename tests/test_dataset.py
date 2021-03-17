31b8b01e25e045eab7323dfb33cdcf4086797f5e
kqf <q.dev.null@gmail.com>
2021-03-17 21:51:55 +0100

    Add the dataset tests (#65)
    * Add the similar dimensions
    
    * Fix the dataset sizes

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,6 +6,10 @@ def test_dataset(fake_dataset):
     df = pd.read_csv(fake_dataset / "train.csv")
     dataset = DetectionDatasetV3(df, fake_dataset)
 
-    for image, targets in dataset:
+    for image, (s1, s2, s3) in dataset:
         assert len(image.shape) == 3, "There are only 3 dimensions"
         assert image.shape[-1] == 3, "There are only 3 channels"
+
+        assert s1.shape == (2, 13, 13, 6)
+        assert s2.shape == (2, 26, 26, 6)
+        assert s3.shape == (2, 52, 52, 6)
