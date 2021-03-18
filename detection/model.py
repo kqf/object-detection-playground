041227137e903fe8b9194e21a242ac4ead48dc3b
kqf <q.dev.null@gmail.com>
2021-03-18 17:44:30 +0100

    Integrate the v3 model (#67)
    * Switch to v3 model
    
    * Remove the hyperparameters

--- detection/model.py
+++ detection/model.py
@@ -2,8 +2,8 @@ import torch
 import skorch
 
 
-from detection.models.v5 import YOLO
-from detection.losses.v5 import CombinedLoss
+from detection.models.v3 import YOLO
+from detection.losses.v3 import CombinedLoss
 
 
 def init(w):
@@ -26,14 +26,14 @@ def build_model(max_epochs=2, logdir=".tmp/", train_split=None):
     #     step_every='batch',
     # )
 
-    hyp = {
-        'box': (1, 0.02, 0.2),  # box loss gain
-        'obj': (1, 0.2, 4.0),  # obj loss gain (scale with pixels)
-        'cls': (1, 0.2, 4.0),  # cls loss gain
-        'obj_pw': (1, 0.5, 2.0),  # obj BCELoss positive_weight
-        'cls_pw': (1, 0.5, 2.0),  # cls BCELoss positive_weight
-        'anchor_t': (1, 2.0, 8.0),  # anchor-multiple threshold
-    }
+    # hyp = {
+    #     'box': (1, 0.02, 0.2),  # box loss gain
+    #     'obj': (1, 0.2, 4.0),  # obj loss gain (scale with pixels)
+    #     'cls': (1, 0.2, 4.0),  # cls loss gain
+    #     'obj_pw': (1, 0.5, 2.0),  # obj BCELoss positive_weight
+    #     'cls_pw': (1, 0.5, 2.0),  # cls BCELoss positive_weight
+    #     'anchor_t': (1, 2.0, 8.0),  # anchor-multiple threshold
+    # }
 
     model = DetectionNet(
         YOLO,
@@ -42,7 +42,7 @@ def build_model(max_epochs=2, logdir=".tmp/", train_split=None):
         max_epochs=max_epochs,
         # optimizer__momentum=0.9,
         criterion=CombinedLoss,
-        criterion__hyp=hyp,
+        # criterion__hyp=hyp,
         iterator_train__shuffle=True,
         iterator_train__num_workers=6,
         iterator_valid__shuffle=False,
