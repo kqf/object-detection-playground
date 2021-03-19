013626876c668ce49fc82d1ec6d188ab109798f6
Sasha <q.dev.null@gmail.com>
2021-03-19 19:29:54 +0100

    Fix the v3 model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,12 @@
-import pytest
-from detection.data import DetectionDataset, read_data
+from detection.data import read_data
+from detection.dataset import DetectionDatasetV3
 from detection.model import build_model
 
 
-@pytest.mark.skip("Fix the inputs mismatch")
 def test_dummy(fake_dataset):
     df = read_data(fake_dataset / "train.csv")
     print(df.head())
-    train = DetectionDataset(df, fake_dataset)
+    train = DetectionDatasetV3(df, fake_dataset)
 
     model = build_model()
     model.fit(train)
