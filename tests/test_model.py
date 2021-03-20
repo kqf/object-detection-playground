1b049d860524897c0157d5b094dae2c2506e7e48
Sasha <q.dev.null@gmail.com>
2021-03-20 06:38:53 +0100

    Add missing augmentations

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,8 @@
-import pytest
+# import pytest
 
 from detection.data import read_data
-from detection.datasets.v3 import DetectionDatasetV3
+from detection.dataset import DetectionDatasetV3
+from detection.augmentations import transform
 from detection.model import build_model
 
 
@@ -9,7 +10,7 @@ from detection.model import build_model
 def test_dummy(fake_dataset):
     df = read_data(fake_dataset / "train.csv")
     print(df.head())
-    train = DetectionDatasetV3(df, fake_dataset)
+    train = DetectionDatasetV3(df, fake_dataset, transforms=transform())
 
     for x in train:
         print(x)
