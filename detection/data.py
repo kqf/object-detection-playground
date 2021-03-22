bfe86f99ea4760dde8e3dfb5458640a3f0d656aa
Sasha <q.dev.null@gmail.com>
2021-03-22 15:00:53 +0100

    Set the random box for empty images

--- detection/data.py
+++ detection/data.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 def read_data(path):
     df = pd.read_csv(path)
-    df.loc[df["class_id"] == 14, ['x_max', 'y_max']] = 1000
-    df.loc[df["class_id"] == 14, ['x_min', 'y_min']] = 0.001
+    df.loc[df["class_id"] == 14, 'x_min'] = 691.0
+    df.loc[df["class_id"] == 14, 'x_max'] = 1653.0
+    df.loc[df["class_id"] == 14, 'y_min'] = 1375.0
+    df.loc[df["class_id"] == 14, 'y_max'] = 1831.0
 
     df["class_id"] = df["class_id"] + 1
     df.loc[df["class_id"] == 15, ["class_id"]] = 0
