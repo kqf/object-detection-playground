ab6874f05d5d36af4adaa4bff78284ce3de3502c
Sasha <q.dev.null@gmail.com>
2021-03-24 19:11:23 +0100

    Plot the rectangle pathces

--- tests/test_visual.py
+++ tests/test_visual.py
@@ -8,7 +8,15 @@ def bbox():
     return [400, 400, 600, 800]
 
 
+def rectangle(x1, y1, x2, y2):
+    w, h = x2 - x1, y2 - y1
+    return plt.Rectangle((x1, y1), w, h, color='r', fill=False)
+
+
 def test_mc(bbox):
     output = blob2image(make_blob(*bbox))
     plt.imshow(output)
+    x1, y1, x2, y2 = bbox
+    ax = plt.gca()
+    ax.add_patch(rectangle(x1, y1, x2, y2))
     plt.show()
