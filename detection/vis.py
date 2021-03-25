dc7a403a1bfa38d04c5933e0a155187ec8d45e7c
kqf <q.dev.null@gmail.com>
2021-03-25 19:17:54 +0100

    Check agumentations (#84)
    * Add the endpoint to check the augmentations
    
    * Fix the visualizations
    
    * Add the visualizations to the test

---
+++ detection/vis.py
@@ -0,0 +1,20 @@
+import click
+import pandas as pd
+import pathlib
+
+from detection.datasets.v3 import DetectionDatasetV3
+from detection.augmentations import transform
+from detection.plot import plot
+
+
+@click.command()
+@click.option("--datapath")
+def main(datapath):
+    path = pathlib.Path(datapath)
+    df = pd.read_csv(path / "train.csv")
+    dataset = DetectionDatasetV3(df, path, transforms=transform(train=True))
+
+    for image, (s1, s2, s3) in dataset:
+        plot([image, s1])
+        plot([image, s2])
+        plot([image, s3])
