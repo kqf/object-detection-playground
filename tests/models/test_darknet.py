b4cd328c0a5622cbca431e1fad58762db62baa1e
Sasha <q.dev.null@gmail.com>
2021-03-30 21:00:38 +0300

    Fix the broken tests

--- tests/models/test_darknet.py
+++ tests/models/test_darknet.py
@@ -21,8 +21,8 @@ def test_module(batch):
 
 def test_legacy(batch):
     model = build_model(3, 40)
-    l1, l2, l3 = model(batch)
+    l1 = model(batch)
 
     assert l1.shape == (64, 1024, 8, 8)
-    assert l2.shape == (64, 512, 16, 16)
-    assert l3.shape == (64, 256, 32, 32)
+    # assert l2.shape == (64, 512, 16, 16)
+    # assert l3.shape == (64, 256, 32, 32)
