e6d64fcf8394a0bf90560d0e04136a1364fa0e28
Sasha <q.dev.null@gmail.com>
2021-03-30 20:55:55 +0300

    Add the tests for the legacy code

--- tests/models/test_darknet.py
+++ tests/models/test_darknet.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from detection.models.darknet import Darknet
+from detection.models.legacy import build_model
 
 
 @pytest.fixture
@@ -11,6 +12,15 @@ def batch():
 
 def test_module(batch):
     model = Darknet()
+    l1, l2, l3 = model(batch)
+
+    assert l1.shape == (64, 1024, 8, 8)
+    assert l2.shape == (64, 512, 16, 16)
+    assert l3.shape == (64, 256, 32, 32)
+
+
+def test_legacy(batch):
+    model = build_model(3, 40)
     l1, l2, l3 = model(batch)
 
     assert l1.shape == (64, 1024, 8, 8)
