2bffc91d2ee33beeaf0e477a1f0464601a0f1f03
kqf <q.dev.null@gmail.com>
2021-04-02 20:29:29 +0200

    Make cache updatable (#104)
    * Rename the layer
    
    * Add the tests
    
    * Fix the broken tests

---
+++ tests/models/test_residual.py
@@ -0,0 +1,14 @@
+import pytest
+import torch
+from detection.models.legacy import SkipConcat
+
+
+@pytest.fixture
+def batch():
+    return torch.rand(32, 3, 224, 224)
+
+
+def test_skips(batch):
+    layer = SkipConcat()
+    x = layer(batch)
+    assert layer.pop() == x
