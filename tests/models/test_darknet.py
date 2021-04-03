c725d7bbdc38b9c0b057b9647868949e5c0d52b1
Sasha <q.dev.null@gmail.com>
2021-04-03 09:09:47 +0300

    Reduce the batch sizes

--- tests/models/test_darknet.py
+++ tests/models/test_darknet.py
@@ -7,25 +7,19 @@ from detection.models.legacy import build_model
 
 @pytest.fixture
 def batch():
-    return torch.rand(64, 3, 256, 256)
+    return torch.rand(4, 3, 256, 256)
 
 
 def test_module(batch):
     model = Darknet()
     l1, l2, l3 = model(batch)
 
-    assert l1.shape == (64, 1024, 8, 8)
-    assert l2.shape == (64, 512, 16, 16)
-    assert l3.shape == (64, 256, 32, 32)
+    assert l1.shape == (4, 1024, 8, 8)
+    assert l2.shape == (4, 512, 16, 16)
+    assert l3.shape == (4, 256, 32, 32)
 
 
-@pytest.mark.skip("Remove this test probably")
 def test_legacy(batch):
     model = torch.nn.Sequential(*build_model(3, 40))
     l1 = model(batch)
-    l2 = model[10].pop()
-    l3 = model[7].pop()
-
-    assert l1.shape == (64, 1024, 8, 8)
-    assert l2.shape == (64, 512, 16, 16)
-    assert l3.shape == (64, 256, 32, 32)
+    assert l1.shape == (4, 1024, 8, 8)
