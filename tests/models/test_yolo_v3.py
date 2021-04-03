a5ec76f644832369a6928249e4a8290772a3922a
Sasha <q.dev.null@gmail.com>
2021-04-03 08:39:43 +0300

    Add the YOLO v3 algorithm

--- tests/models/test_yolo_v3.py
+++ tests/models/test_yolo_v3.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from detection.models.v3 import YOLO
+from detection.models.legacy import YOLO as LOLO
 
 
 @pytest.fixture
@@ -12,6 +13,15 @@ def batch():
 @pytest.mark.skip("Fix the structure of this model")
 def test_module(batch):
     model = YOLO()
+    s1, s2, s3 = model(batch)
+
+    assert s1.shape == (16, 3, 8, 8, 85)
+    assert s2.shape == (16, 3, 16, 16, 85)
+    assert s3.shape == (16, 3, 32, 32, 85)
+
+
+def test_legacy_module(batch):
+    model = LOLO()
     s1, s2, s3 = model(batch)
 
     assert s1.shape == (16, 3, 8, 8, 85)
