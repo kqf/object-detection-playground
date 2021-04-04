1c7e423445912666353c27013930d256448b474b
Sasha <q.dev.null@gmail.com>
2021-04-04 08:00:49 +0300

    Add the parametrized tests for darknet

--- tests/models/test_darknet.py
+++ tests/models/test_darknet.py
@@ -6,20 +6,32 @@ from detection.models.legacy import build_model
 
 
 @pytest.fixture
-def batch():
-    return torch.rand(4, 3, 256, 256)
+def batch(size):
+    return torch.rand(4, 3, size, size)
 
 
-def test_module(batch):
+@pytest.mark.parametrize("size", [
+    32,
+    32 * 2,
+    32 * 10,
+])
+def test_module(batch, size):
     model = Darknet()
     l1, l2, l3 = model(batch)
 
-    assert l1.shape == (4, 1024, 8, 8)
-    assert l2.shape == (4, 512, 16, 16)
-    assert l3.shape == (4, 256, 32, 32)
+    out_size = size // 32
+    assert l1.shape == (4, 1024, out_size, out_size)
+    assert l2.shape == (4, 512, out_size * 2, out_size * 2)
+    assert l3.shape == (4, 256, out_size * 4, out_size * 4)
 
 
-def test_legacy(batch):
+@pytest.mark.parametrize("size", [
+    32,
+    32 * 2,
+    32 * 10,
+])
+def test_legacy(batch, size):
     model = torch.nn.Sequential(*build_model(3, 40))
     l1 = model(batch)
-    assert l1.shape == (4, 1024, 8, 8)
+    out_size = size // 32
+    assert l1.shape == (4, 1024, out_size, out_size)
