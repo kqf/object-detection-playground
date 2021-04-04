29a6e23b2b5f0c7191bced8b60168510fad4875f
Sasha <q.dev.null@gmail.com>
2021-04-04 07:53:39 +0300

    Update the yolov3 tests

--- tests/models/test_yolo_v3.py
+++ tests/models/test_yolo_v3.py
@@ -6,8 +6,8 @@ from detection.models.legacy import YOLO as LOLO
 
 
 @pytest.fixture
-def batch():
-    return torch.rand(4, 3, 416, 416)
+def batch(size):
+    return torch.rand(4, 3, size, size)
 
 
 @pytest.mark.skip("Fix the structure of this model")
@@ -20,10 +20,18 @@ def test_module(batch):
     assert s3.shape == (4, 3, 32, 32, 85)
 
 
-def test_legacy_module(batch):
+@pytest.mark.parametrize("size", [
+    32,
+    32 * 2,
+    32 * 10,
+    460,
+])
+def test_legacy_module(batch, size):
     model = LOLO()
     s1, s2, s3 = model(batch)
 
-    assert s1.shape == (4, 85, 13, 13, 3)
-    assert s2.shape == (4, 85, 26, 26, 3)
-    assert s3.shape == (4, 85, 52, 52, 3)
+    out_size = size // 32
+
+    assert s1.shape == (4, 85, out_size, out_size, 3)
+    assert s2.shape == (4, 85, out_size * 2, out_size * 2, 3)
+    assert s3.shape == (4, 85, out_size * 4, out_size * 4, 3)
