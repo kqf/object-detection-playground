01a15ea22d4ce2d163fbd84afedf0bc8509723ff
kqf <q.dev.null@gmail.com>
2021-04-04 07:26:43 +0200

    Fix the channel ordering (#110)
    * Fix the detection scales ordering for v3 model
    
    * Fix the test definitions for v3 model
    
    * Fix the broken tests

--- detection/models/v3.py
+++ detection/models/v3.py
@@ -17,6 +17,8 @@ def cblock(in_channels, out_channels):
 class ScalePrediction(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
+        self.n_preds = num_classes + 5
+        self.n_scales = 3
         self.pred = nn.Sequential(
             conv(in_channels // 2, in_channels, kernel_size=3, padding=1),
             torch.nn.Conv2d(in_channels, (num_classes + 5) * 3, kernel_size=1),
@@ -24,11 +26,11 @@ class ScalePrediction(nn.Module):
         self.num_classes = num_classes
 
     def forward(self, x):
+        b, _, h, w = x.shape
+
         out = self.pred(x)
         return (
-            out.reshape(
-                x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3]
-            )
+            out.reshape(b, self.n_preds, self.n_scales, h, w)
             .permute(0, 1, 3, 4, 2)
         )
 
@@ -45,21 +47,21 @@ class YOLO(nn.Module):
         )
 
         self.upsample2 = torch.nn.Sequential(
-            conv(1024 // 2, 256, kernel_size=1, stride=1, padding=1),
+            conv(1024 // 2, 256, kernel_size=1, stride=1, padding=0),
             torch.nn.Upsample(scale_factor=2),
         )
         self.conv2 = torch.nn.Sequential(
-            conv(256 * 3, 256, kernel_size=1, stride=1, padding=1),
+            conv(256 * 3, 256, kernel_size=1, stride=1, padding=0),
             conv(256, 512, kernel_size=3, stride=1, padding=1),
             cblock(512, 512 // 2),
         )
 
         self.upsample3 = torch.nn.Sequential(
-            conv(256, 128, kernel_size=1, stride=1, padding=1),
+            conv(256, 128, kernel_size=1, stride=1, padding=0),
             torch.nn.Upsample(scale_factor=2),
         )
         self.conv3 = torch.nn.Sequential(
-            conv(128 * 3, 128, kernel_size=1, stride=1, padding=1),
+            conv(128 * 3, 128, kernel_size=1, stride=1, padding=0),
             conv(128, 256, kernel_size=3, stride=1, padding=1),
             cblock(256, 256 // 2),
         )
--- tests/models/test_yolo_v3.py
+++ tests/models/test_yolo_v3.py
@@ -10,14 +10,20 @@ def batch(size):
     return torch.rand(4, 3, size, size)
 
 
-@pytest.mark.skip("Fix the structure of this model")
-def test_module(batch):
+@pytest.mark.parametrize("size", [
+    32,
+    32 * 2,
+    32 * 10,
+])
+def test_module(batch, size):
     model = YOLO()
     s1, s2, s3 = model(batch)
 
-    assert s1.shape == (4, 3, 8, 8, 85)
-    assert s2.shape == (4, 3, 16, 16, 85)
-    assert s3.shape == (4, 3, 32, 32, 85)
+    out_size = size // 32
+
+    assert s1.shape == (4, 85, out_size, out_size, 3)
+    assert s2.shape == (4, 85, out_size * 2, out_size * 2, 3)
+    assert s3.shape == (4, 85, out_size * 4, out_size * 4, 3)
 
 
 @pytest.mark.parametrize("size", [
