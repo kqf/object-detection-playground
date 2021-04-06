3dd7ce41e7ca6d379a28ea2b2f30963434366d81
kqf <q.dev.null@gmail.com>
2021-04-06 17:16:06 +0200

    Add generate dataset (#114)
    * Create the data specific method for object detection
    
    * Add the separate function to generate the data

--- detection/mc.py
+++ detection/mc.py
@@ -1,4 +1,8 @@
+import cv2
+import pandas as pd
 import numpy as np
+
+from pathlib import Path
 
 
 def make_blob(
@@ -33,3 +37,30 @@ def blob2image(blob, channels=3, epsilon=0.1):
 
     # Convet to image scale
     return (extended + noise * 255).astype(np.uint8)
+
+
+def annotations(n_points=1000, h=2000, w=2000):
+    x = np.random.uniform(0, w, (n_points, 2))
+    y = np.random.uniform(0, h, (n_points, 2))
+    df = pd.DataFrame({"image_id": np.arange(n_points)})
+    df["image_id"] = [1, 2, 3, 4, 5],
+
+    df["x_min"] = x.min(axis=1)
+    df["y_min"] = y.min(axis=1)
+
+    df["x_max"] = x.max(axis=0)
+    df["y_max"] = x.max(axis=0)
+    labels = (df["x_max"] - df["x_min"]) > (df["y_max"] - df["y_min"])
+    df["class_id"] = labels.astype(int)
+    df["class_name"] = labels.astype(str)
+    return df
+
+
+def generate_to_directory(annotations, dirname):
+    path = Path(dirname)
+    for row in annotations.to_dict(orient="records"):
+        image_id = row["image_id"]
+        img = blob2image(make_blob(**row))
+        ifile = f"{image_id}.png"
+        cv2.imwrite(str(path / ifile), img)
+    annotations.to_csv(path / "train.csv", index=False)
