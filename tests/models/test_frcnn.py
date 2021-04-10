864ba94b0eea74dbbeb221af5a2da479a0877bba
Sasha <q.dev.null@gmail.com>
2021-04-10 13:59:54 +0200

    Fix the fast rcnn tests

--- tests/models/test_frcnn.py
+++ tests/models/test_frcnn.py
@@ -16,10 +16,5 @@ def batch(size):
 ])
 def test_module(batch, size):
     model = FasterRCNN()
-    s1, s2, s3 = model(batch)
-
-    out_size = size // 32
-
-    assert s1.shape == (4, 85, out_size, out_size, 3)
-    assert s2.shape == (4, 85, out_size * 2, out_size * 2, 3)
-    assert s3.shape == (4, 85, out_size * 4, out_size * 4, 3)
+    output = model(batch)
+    assert output.shape == (4, 3, size, size)
