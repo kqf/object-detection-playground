b7a012d3a361dc694db713a2ea82f3934c26af05
Sasha <q.dev.null@gmail.com>
2021-04-11 09:45:21 +0200

    Fix the test parameters

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -10,7 +10,16 @@ def test_loss():
     target[..., 1] = 0.5
     target[..., 2] = 0.5
     target[..., 3:5] = DEFAULT_ANCHORS[0][0]
+
     predictions = torch.zeros([1, 85, 13, 13, 3])
+    predictions = predictions.transpose(1, -1)
+    predictions[..., 0] = 0.9999
+
+    # predictions go through the sigmoid function
+    predictions[..., 1] = torch.logit(0.5)
+    predictions[..., 2] = torch.logit(0.5)
+    predictions[..., 3] = 0
+    predictions[..., 4] = 0
 
     criterion = CombinedLoss(DEFAULT_ANCHORS)
     loss = criterion._forward(predictions, target, DEFAULT_ANCHORS[0])
