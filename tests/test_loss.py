6050f6e87c05e6d29f344bd2b2e0980f46c9aa7e
Sasha <q.dev.null@gmail.com>
2021-04-11 09:51:03 +0200

    Finalize the test examples

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -10,16 +10,26 @@ def test_loss():
     target[..., 1] = 0.5
     target[..., 2] = 0.5
     target[..., 3:5] = DEFAULT_ANCHORS[0][0]
+    # NB: class label is zero, this corresponds to the first class
+    #     the prediction index for the first class is 5 = 1 (is obj) + 4 coords
+    target[..., 5] = 0
 
     predictions = torch.zeros([1, 85, 13, 13, 3])
     predictions = predictions.transpose(1, -1)
     predictions[..., 0] = 0.9999
 
     # predictions go through the sigmoid function
-    predictions[..., 1] = torch.logit(0.5)
-    predictions[..., 2] = torch.logit(0.5)
+    predictions[..., 1] = torch.logit(torch.tensor(0.5))
+    predictions[..., 2] = torch.logit(torch.tensor(0.5))
+
+    # It should be scaled through the exp
     predictions[..., 3] = 0
     predictions[..., 4] = 0
+
+    # Set the proper label
+    predictions[..., 5] = 1
+    # Transpose back
+    predictions = predictions.transpose(1, -1)
 
     criterion = CombinedLoss(DEFAULT_ANCHORS)
     loss = criterion._forward(predictions, target, DEFAULT_ANCHORS[0])
