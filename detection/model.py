5300e6271e606a59c0ac10e123618c443c6de3d6
Sasha <q.dev.null@gmail.com>
2021-04-18 09:29:39 +0200

    Fix the predictions shapes

--- detection/model.py
+++ detection/model.py
@@ -3,7 +3,7 @@ import skorch
 import numpy as np
 
 
-# from functools import partial
+from functools import partial
 
 from detection.models.v3 import YOLO
 from detection.losses.v3 import CombinedLoss
@@ -34,17 +34,16 @@ def infer(batch, anchor_boxes):
     predictions = []
 
     for i, (pred, achors) in enumerate(zip(batch, anchor_boxes)):
+        # [batch, scale, x, y, labels] -> [batch, x, y, scale, labels]
+        pred = pred.permute(0, 2, 3, 4, 1)
+
         # Copy don't mutate the original batch
-        prediction = batch[..., :6].detach().clone()
+        prediction = pred[..., :6].detach().clone()
 
         # pred [batch_size, n_anchors, s, s, 5 + nclasses]
         scale = pred.shape[2]
 
         prediction[..., 0:2] = torch.sigmoid(pred[..., 0:2])
-        import ipdb
-        ipdb.set_trace()
-        import IPython
-        IPython.embed()  # noqa
         prediction[..., 2:5] = torch.exp(pred[..., 2:5]) * anchor_boxes * scale
         prediction[..., 0] = torch.sigmoid(pred[..., 0])
         prediction[..., 5] = torch.argmax(pred[..., 5:], dim=-1).unsqueeze(-1)
@@ -61,7 +60,7 @@ class DetectionNet(skorch.NeuralNet):
         nonlin = self._get_predict_nonlinearity()
         y_probas = []
         for yp in self.forward_iter(X, training=False):
-            yp = nonlin(yp[0])
+            yp = nonlin(yp)
             y_probas.append(skorch.utils.to_numpy(yp))
         y_proba = np.concatenate(y_probas, 0)
         return y_proba
@@ -90,7 +89,7 @@ def build_model(max_epochs=2, logdir=".tmp/", train_split=None):
         iterator_valid__shuffle=False,
         iterator_valid__num_workers=6,
         train_split=train_split,
-        # predict_nonlinearity=partial(infer, anchor_boxes=DEFAULT_ANCHORS),
+        predict_nonlinearity=partial(infer, anchor_boxes=DEFAULT_ANCHORS),
         callbacks=[
             skorch.callbacks.ProgressBar(),
             # skorch.callbacks.Checkpoint(dirname=logdir),
