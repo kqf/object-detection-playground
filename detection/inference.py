7af616e2b3954b6257d586e727b24cce60fea96e
Sasha <q.dev.null@gmail.com>
2021-04-21 17:59:20 +0200

    Remove the NMS module

--- detection/inference.py
+++ detection/inference.py
@@ -1,4 +1,5 @@
 import torch
+from detection.metrics import bbox_iou
 
 
 def to_global(x, scale):
@@ -47,3 +48,26 @@ def merge_scales(predictions):
 
     # The results along the batch dimension
     return [torch.cat(x) for x in zip(*flat)]
+
+
+def iou(box1, box2):
+    return bbox_iou(torch.tensor(box1[2:]), torch.tensor(box2[2:]))
+
+
+def non_max_suppression(bboxes, iou_threshold, threshold):
+    bboxes = [box for box in bboxes if box[1] > threshold]
+    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes_after_nms = []
+
+    while bboxes:
+        chosen_box = bboxes.pop(0)
+
+        bboxes = [
+            box
+            for box in bboxes
+            if box[0] != chosen_box[0] or iou(chosen_box, box) < iou_threshold
+        ]
+
+        bboxes_after_nms.append(chosen_box)
+
+    return bboxes_after_nms
