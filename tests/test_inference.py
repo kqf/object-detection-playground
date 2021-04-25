235baabb51ace08ceaed88536f395296eba5453a
kqf <q.dev.null@gmail.com>
2021-04-25 10:26:24 +0200

    Add meaningful inference tests (#149)
    * Generate batch prediction
    
    * Convert cell local coordinates to the global ones
    
    * Add the global scale conversion
    
    * Remove the unused fixture
    
    * Simplify the local -> global conversion

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,18 +1,30 @@
 import torch
 import pytest
 
-from detection.inference import infer, nms
+from detection.inference import infer, nms, merge_scales
 from detection.plot import plot
 from detection.datasets.v3 import DEFAULT_ANCHORS
 
 
 @pytest.fixture
-def batch(bsize):
-    scales = [
-        torch.zeros([bsize, 85, 13, 13, 3]),
-        torch.zeros([bsize, 85, 26, 26, 3]),
-        torch.zeros([bsize, 85, 52, 52, 3]),
-    ]
+def batch(bsize, scale=13):
+    scales = []
+
+    for i, n in enumerate([scale, scale * 2, scale * 4]):
+        x = torch.zeros([bsize, 85, n, n, 3])
+        x_cells = torch.arange(n).reshape(1, 1, n, 1, 1)
+        y_cells = torch.arange(n).reshape(1, 1, 1, n, 1)
+
+        x[:, 0] = torch.zeros(1, n, n, 3) + 0.9
+        x[:, 1] = (torch.zeros(1, n, n, 3) + 0.5 + x_cells) / n
+        x[:, 2] = (torch.zeros(1, n, n, 3) + 0.5 + y_cells) / n
+        x[:, 3] = 0.01
+        x[:, 4] = 0.01
+        x[:, 5] = 1
+
+        # Append the global predictions
+        scales.append(x)
+
     return scales
 
 
@@ -27,18 +39,8 @@ def test_inference(batch, bsize):
         nms(sample)
 
 
-@pytest.fixture
-def merged_batch(predictions_size=6):
-    preds = torch.zeros(predictions_size, 6)
-    preds[:, 0] = torch.linspace(0, 0.99, predictions_size)
-    preds[:, 1] = torch.linspace(0.4, 0.6, predictions_size)
-    preds[:, 2] = torch.linspace(0.4, 0.6, predictions_size)
-    preds[:, 3] = 0.4
-    preds[:, 4] = 0.4
-    return preds
-
-
-@pytest.mark.skip("Implement me")
-def test_nms(merged_batch):
+@pytest.mark.parametrize("bsize", [4])
+def test_nms(batch, bsize=10):
+    merged_batch = merge_scales([x.permute(0, 2, 3, 4, 1) for x in batch])
     img = torch.ones(3, 460, 460)
-    plot((img, [x[1:5] for x in merged_batch]), convert_bbox=True)
+    plot((img, [x[1:5] for x in merged_batch[0]]), convert_bbox=True)
