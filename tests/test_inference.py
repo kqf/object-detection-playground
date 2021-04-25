cf8abcc53b0bd27180ca5d62ef625b04c3f4171b
Sasha <q.dev.null@gmail.com>
2021-04-25 08:37:45 +0200

    Generate batch prediction

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -7,12 +7,18 @@ from detection.datasets.v3 import DEFAULT_ANCHORS
 
 
 @pytest.fixture
-def batch(bsize):
-    scales = [
-        torch.zeros([bsize, 85, 13, 13, 3]),
-        torch.zeros([bsize, 85, 26, 26, 3]),
-        torch.zeros([bsize, 85, 52, 52, 3]),
-    ]
+def batch(bsize, scale=13):
+    scales = []
+
+    for n in [scale, scale * 2, scale * 4]:
+        x = torch.zeros([bsize, 85, n, n, 3])
+        x[:, 0] = torch.linspace(0, 0.9, n * n * 3).reshape(1, n, n, 3)
+        x[:, 1] = torch.linspace(0.4, 0.6, n * n * 3).reshape(1, n, n, 3)
+        x[:, 2] = torch.linspace(0.4, 0.6, n * n * 3).reshape(1, n, n, 3)
+        x[:, 3] = 0.2
+        x[:, 4] = 0.2
+        scales.append(x)
+
     return scales
 
 
@@ -33,12 +39,12 @@ def merged_batch(predictions_size=6):
     preds[:, 0] = torch.linspace(0, 0.99, predictions_size)
     preds[:, 1] = torch.linspace(0.4, 0.6, predictions_size)
     preds[:, 2] = torch.linspace(0.4, 0.6, predictions_size)
-    preds[:, 3] = 0.4
-    preds[:, 4] = 0.4
+    preds[:, 3] = 0.2
+    preds[:, 4] = 0.2
     return preds
 
 
-@pytest.mark.skip("Implement me")
+@pytest.mark.skip
 def test_nms(merged_batch):
     img = torch.ones(3, 460, 460)
     plot((img, [x[1:5] for x in merged_batch]), convert_bbox=True)
