12f9ec27ac666e98e6bb5a59377a902e3e3d0022
kqf <q.dev.null@gmail.com>
2021-05-01 13:04:58 +0200

    Fix the generated images (#159)
    * Fix the same coordinates for all bboxes
    
    * Ensure the same annotations for all examples
    
    * Ensure the same label

--- tests/conftest.py
+++ tests/conftest.py
@@ -58,6 +58,12 @@ def annotations():
     df.loc[df["class_id"] == 14, 'x_max'] = 1653.0
     df.loc[df["class_id"] == 14, 'y_min'] = 1375.0
     df.loc[df["class_id"] == 14, 'y_max'] = 1831.0
+
+    df.loc[:, 'x_min'] = 200.0
+    df.loc[:, 'x_max'] = 200.0 + 2000 * 0.28
+    df.loc[:, 'y_min'] = 400.0
+    df.loc[:, 'y_max'] = 400.0 + 2000 * 0.22
+    df.loc[:, "class_id"] = 1
     return df
 
 
