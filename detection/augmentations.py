83b1ef465df097c13d6b0301ecefcefabdf65629
kqf <q.dev.null@gmail.com>
2021-05-08 22:10:03 +0300

    Turn back augmentations (#171)
    * Turn back the augmentations
    
    * Add back the Padding if needed

--- detection/augmentations.py
+++ detection/augmentations.py
@@ -1,4 +1,4 @@
-# import cv2
+import cv2
 import warnings
 import albumentations as alb
 from albumentations.pytorch import ToTensorV2
@@ -26,7 +26,10 @@ class DebugAugmentations(DualTransform):
         return img
 
     def apply_to_bbox(self, bbox, **params):
-        import ipdb; ipdb.set_trace(); import IPython; IPython.embed() # noqa
+        import ipdb
+        ipdb.set_trace()
+        import IPython
+        IPython.embed()  # noqa
         return bbox
 
     def apply_to_keypoint(self, keypoint, scale=0, **params):
@@ -44,11 +47,11 @@ def transform(train=True, mean=None, std=None, size=32 * 13):
         warnings.warn(f"Image shape should be a multiple of 32, got {size}")
 
     transforms = [
-        # alb.PadIfNeeded(
-        #     min_height=int(size * scale),
-        #     min_width=int(size * scale),
-        #     border_mode=cv2.BORDER_CONSTANT,
-        # ),
+        alb.PadIfNeeded(
+            min_height=int(size),
+            min_width=int(size),
+            border_mode=cv2.BORDER_CONSTANT,
+        ),
         # DebugAugmentations(),
         alb.Resize(size, size),
         alb.Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
@@ -58,7 +61,7 @@ def transform(train=True, mean=None, std=None, size=32 * 13):
     train_transforms = []
     if train:
         train_transforms = [
-            # alb.Flip(0.5)
+            alb.Flip(0.5)
         ]
 
     return alb.Compose(train_transforms + transforms, bbox_params=bbox_params)
