56ce615b1aae03c2ad4193bc8a1e9feb297a658c
kqf <q.dev.null@gmail.com>
2021-05-18 07:37:03 +0300

    Add v4 downsample5 (#193)
    * Import the module
    
    * Add the activation
    
    * Add the tests for downsample5

--- detection/models/v4.py
+++ detection/models/v4.py
@@ -158,6 +158,30 @@ class DownSample4(torch.nn.Module):
         return x5
 
 
+class DownSample5(torch.nn.Module):
+    def __init__(self, activation=torch.nn.LeakyReLU):
+        super().__init__()
+        self.conv1 = Conv(512, 1024, 3, 2, activation())
+        self.conv2 = Conv(1024, 512, 1, 1, activation())
+        self.conv3 = Conv(1024, 512, 1, 1, activation())
+
+        self.resblock = resblock(ch=512, nblocks=4)
+        self.conv4 = Conv(512, 512, 1, 1, activation())
+        self.conv5 = Conv(1024, 1024, 1, 1, activation())
+
+    def forward(self, input):
+        x1 = self.conv1(input)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x1)
+
+        r = self.resblock(x3)
+        x4 = self.conv4(r)
+
+        x4 = torch.cat([x4, x2], dim=1)
+        x5 = self.conv5(x4)
+        return x5
+
+
 class Head(torch.nn.Module):
     def __init__(self, output_ch, n_classes):
         super().__init__()
