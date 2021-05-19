c61305f48e86628ac18e2a7d8a7b45396827f2eb
Sasha <q.dev.null@gmail.com>
2021-05-19 20:08:52 +0300

    Simplify Mish

--- detection/models/v4.py
+++ detection/models/v4.py
@@ -4,9 +4,6 @@ from detection.models.darknet import Residual
 
 
 class Mish(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
     def forward(self, x):
         x = x * (torch.tanh(torch.nn.functional.softplus(x)))
         return x
