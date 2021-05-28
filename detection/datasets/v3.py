ab63b324e49982a4956108390a70f95e70cd2be6
Sasha <q.dev.null@gmail.com>
2021-05-28 07:01:06 +0300

    Simplify the detection datasets

--- detection/datasets/v3.py
+++ detection/datasets/v3.py
@@ -33,9 +33,11 @@ class DetectionDatasetV3(Dataset):
         self.image_dir = image_dir
         self.transforms = transforms
 
-        anchors = anchors or DEFAULT_ANCHORS
+        if anchors is None:
+            anchors = DEFAULT_ANCHORS
+
         self.num_anchors_per_scale = len(anchors)
-        self.anchors = anchors or torch.cat(anchors)
+        self.anchors = torch.cat(anchors)
         self.iou_threshold = iou_threshold
         self.scales = scales or DEFAULT_SCALES
 
