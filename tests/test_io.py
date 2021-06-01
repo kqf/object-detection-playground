98be9177d5364aabd84a9c99e19f37bf6de36e5a
Sasha <q.dev.null@gmail.com>
2021-06-01 15:58:31 +0300

    Fix the tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,17 +1,32 @@
+import pytest
 
 import click
 
 from click.testing import CliRunner
-from detection.io import ClickRemotePath
+from unittest.mock import patch
+from detection.io import ClickAnyPath
 
 
-def test_handles_remote_path():
+@pytest.mark.parametrize("file, exists, ls, exit_code", [
+    ("gs://simple/path.txt", True, ["simple/path.txt"], 0,),  # Simple GCS
+    ("gs://simple/path.txx", False, ["simple/path.txt"], 2,),
+    ("gs://simple/path.txt", False, [], 2,),  # Error arent exist
+])
+@pytest.mark.parametrize("option", [True, False])
+@patch("detection.io.gcsfs.GCSFileSystem.exists")
+@patch("detection.io.gcsfs.GCSFileSystem.ls", autospec=True)
+def test_handles_remote_path(patch_ls, patch_exists,
+                             file, exists, ls, exit_code, option):
+    patch_ls.return_value = ls
+    patch_exists.return_value = exists
+
     @click.command()
-    @click.option('--datapath', type=ClickRemotePath(exists=False),
+    @click.option('--datapath', type=ClickAnyPath(exists=option),
                   required=True)
     def entrypoint(datapath):
         pass
 
     runner = CliRunner()
-    result = runner.invoke(entrypoint, ['--datapath', "nothing"])
-    assert result.exit_code == 0
+    result = runner.invoke(entrypoint, ['--datapath', file])
+    # print("The output", result.output)
+    assert result.exit_code == 0 if option + exists > 1 else 2
