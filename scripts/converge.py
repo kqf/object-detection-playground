bca4ee560d84dd43e0f34fc976930fa8797c9932
kqf <q.dev.null@gmail.com>
2021-06-01 09:02:29 +0300

    Add a simple script to test convergence (#227)
    * Add a simple script to test convergence
    
    * Modify the converge script to reflect the regression

---
+++ scripts/converge.py
@@ -0,0 +1,21 @@
+import torch
+
+
+def main():
+    y = torch.as_tensor([0.2, 0.9]).float()
+    y_hat = torch.randn(2, requires_grad=True)
+    n_ephoch = 100
+    lr = 0.1
+    optimizer = torch.optim.Adam([y_hat], lr=lr)
+    for i in range(n_ephoch):
+        optimizer.zero_grad()
+        loss = ((y - y_hat) ** 2).mean()
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {i}, loss {loss.item()}")
+
+    print(y, y_hat)
+
+
+if __name__ == '__main__':
+    main()
