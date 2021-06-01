88b56f0d0a27b0b5c33c60d5c0b6b67c6ec8cff3
kqf <q.dev.null@gmail.com>
2021-06-01 15:59:02 +0300

    Add the click Cloud io interface (#228)
    * Add the balisc functionality
    
    * Fix the tests
    
    * Fix the tests

---
+++ detection/io.py
@@ -0,0 +1,69 @@
+import os
+import click
+import gcsfs
+
+import typing as t
+from pathlib import Path
+
+
+def ls(path, gs_prefix="gs://"):
+    if not path.startswith(gs_prefix):
+        return os.listdir(path)
+
+    fs = gcsfs.GCSFileSystem()
+    return [f"{gs_prefix}{f}" for f in fs.ls(path)]
+
+
+def parent(path, gs_prefix="gs://"):
+    if not path.startswith(gs_prefix):
+        return Path(path).parent
+
+    posixpath = Path(path[len(gs_prefix):]).parent
+    return os.path.join(gs_prefix, posixpath)
+
+
+class ClickAnyPath(click.Path):
+    _gs_prefix = "gs://"
+
+    def convert(
+        self,
+        value: t.Any,
+        param: t.Optional["click.Parameter"],
+        ctx: t.Optional["click.Context"],
+    ) -> t.Any:
+        if not value.startswith(self._gs_prefix):
+            return super().convert(value, param, ctx)
+
+        fs = gcsfs.GCSFileSystem()
+        exists = fs.exists(value)
+
+        if exists:
+            return value
+
+        # Proceed even if not exists
+        if not exists and not self.exists:
+            return value
+
+        try:
+            filenames = ls(parent(value, self._gs_prefix))
+            content = f"\n".join(filenames)
+            parent_content = f"The parent folder contains:{content}"
+        except FileNotFoundError:
+            parent_content = f"Parent folder does not exit either"
+
+        self.fail(
+            f"The entity \n{value}\n does not exit. {parent_content}",
+            param,
+            ctx
+        )
+        return value
+
+
+@click.command()
+@click.option('--datapath', type=ClickAnyPath(exists=True), required=True)
+def entrypoint(datapath):
+    print("Datapath:", datapath)
+
+
+if __name__ == '__main__':
+    entrypoint()
