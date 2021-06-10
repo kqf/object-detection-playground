0811c03e4d475976638134c4bb8a5e7c7b794e0a
Sasha <q.dev.null@gmail.com>
2021-06-10 08:02:05 +0300

    Fix the naming

--- scripts/converge.py
+++ scripts/converge.py
@@ -3,18 +3,18 @@ import torch
 
 def main():
     y = torch.as_tensor([0.2, 0.9]).float()
-    y_hat = torch.randn(2, requires_grad=True)
+    logits = torch.randn(2, requires_grad=True)
     n_ephoch = 100
     lr = 0.1
-    optimizer = torch.optim.Adam([y_hat], lr=lr)
+    optimizer = torch.optim.Adam([logits], lr=lr)
     for i in range(n_ephoch):
         optimizer.zero_grad()
-        loss = ((y - torch.exp(y_hat) * y) ** 2).mean()
+        loss = ((y - torch.exp(logits) * y) ** 2).mean()
         loss.backward()
         optimizer.step()
         print(f"Epoch {i}, loss {loss.item()}")
 
-    print(y, torch.exp(y_hat) * y)
+    print(y, torch.exp(logits) * y)
 
 
 if __name__ == '__main__':
