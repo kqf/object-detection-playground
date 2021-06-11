d1600109c68a8e6e6ae2b77014564ab108601d10
kqf <q.dev.null@gmail.com>
2021-06-11 08:06:08 +0300

    Adjust the convergence script (#252)
    * Fix the naming
    
    * Ensure the proper transformations
    
    * Fix the convergence

--- scripts/converge.py
+++ scripts/converge.py
@@ -2,19 +2,23 @@ import torch
 
 
 def main():
-    y = torch.as_tensor([0.2, 0.9]).float()
-    y_hat = torch.randn(2, requires_grad=True)
-    n_ephoch = 100
+    n_grids = 13
+    y = torch.as_tensor([0.28, 0.22]).float() * n_grids
+    logits = torch.randn(2, requires_grad=True)
+    n_ephoch = 1000
     lr = 0.1
-    optimizer = torch.optim.Adam([y_hat], lr=lr)
+    optimizer = torch.optim.Adam([logits], lr=lr)
     for i in range(n_ephoch):
         optimizer.zero_grad()
-        loss = ((y - torch.exp(y_hat) * y) ** 2).mean()
+
+        trues = torch.log(1e-16 + y / y)
+        preds = logits
+        loss = ((trues - preds) ** 2).mean()
         loss.backward()
         optimizer.step()
         print(f"Epoch {i}, loss {loss.item()}")
 
-    print(y, torch.exp(y_hat) * y)
+    print(y, torch.exp(logits) * y / n_grids)
 
 
 if __name__ == '__main__':
