b7ed6a7be7c8480bebe5708db878c7bcb56c9fb1
Sasha <q.dev.null@gmail.com>
2021-06-11 08:01:41 +0300

    Ensure the proper transformations

--- scripts/converge.py
+++ scripts/converge.py
@@ -4,12 +4,15 @@ import torch
 def main():
     y = torch.as_tensor([0.2, 0.9]).float()
     logits = torch.randn(2, requires_grad=True)
-    n_ephoch = 100
+    n_ephoch = 1000
     lr = 0.1
     optimizer = torch.optim.Adam([logits], lr=lr)
     for i in range(n_ephoch):
         optimizer.zero_grad()
-        loss = ((y - torch.exp(logits) * y) ** 2).mean()
+
+        trues = torch.log(1e-16 + y / y)
+        preds = logits
+        loss = ((trues - preds) ** 2).mean()
         loss.backward()
         optimizer.step()
         print(f"Epoch {i}, loss {loss.item()}")
