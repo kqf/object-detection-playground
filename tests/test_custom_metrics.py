0fc879fd6ef33df18e35b3949df002ab30142106
Sasha <q.dev.null@gmail.com>
2021-06-12 09:33:57 +0300

    Add the new cases to check

--- tests/test_custom_metrics.py
+++ tests/test_custom_metrics.py
@@ -5,8 +5,10 @@ from torch import tensor as tt
 from detection.metrics import bbox_iou
 
 
-@pytest.mark.parametrize("gt, pred, answer", [
+@pytest.mark.parametrize("pred, gt, answer", [
     (tt([0.5, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 1.0),
+    # TODO: Check me
+    # (tt([0.5, 1.0, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 0.5),
 ])
 def test_iou(gt, pred, answer):
     print(bbox_iou(pred, gt).item(), answer)
