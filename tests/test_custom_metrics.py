ddee711573255bac64623b6237e893c4c6b3c0c1
Sasha <q.dev.null@gmail.com>
2021-06-13 08:46:23 +0300

    Add more test cases

--- tests/test_custom_metrics.py
+++ tests/test_custom_metrics.py
@@ -7,9 +7,10 @@ from detection.plot import check_boxes
 
 
 @pytest.mark.parametrize("pred, gt, answer", [
-    # (tt([0.5, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 1.0),
-    # TODO: Check me
-    (tt([0.75, 0.5, 0.5, 0.5]), tt([0.5, 0.5, 0.5, 0.5]), 0.5),
+    (tt([0.5, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 1.0),
+    (tt([1.0, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 0.3333333),
+    (tt([1.0, 1.0, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 0.25 / (2 - 0.25)),
+    (tt([1.0, 1.0, 0.5, 0.5]), tt([0.5, 0.5, 0.5, 0.5]), 0),
 ])
 def test_iou(gt, pred, answer):
     print(bbox_iou(pred, gt).item(), answer)
