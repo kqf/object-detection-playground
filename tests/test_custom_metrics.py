2ef2711a0c30d24aaeede4bc0fa7d700f4e7de75
kqf <q.dev.null@gmail.com>
2021-06-13 09:05:40 +0300

    Debug iou metrics (#258)
    * Add a script to plot the metrics
    
    * Fix the plot
    
    * Apply the plot function
    
    * Fix the fill
    
    * Add more test cases
    
    * Comment out the plotting

--- tests/test_custom_metrics.py
+++ tests/test_custom_metrics.py
@@ -3,13 +3,16 @@ import pytest
 import torch
 from torch import tensor as tt
 from detection.metrics import bbox_iou
+# from detection.plot import check_boxes
 
 
 @pytest.mark.parametrize("pred, gt, answer", [
     (tt([0.5, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 1.0),
-    # TODO: Check me
-    # (tt([1, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 0.5),
+    (tt([1.0, 0.5, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 0.3333333),
+    (tt([1.0, 1.0, 1.0, 1.0]), tt([0.5, 0.5, 1.0, 1.0]), 0.25 / (2 - 0.25)),
+    (tt([1.0, 1.0, 0.5, 0.5]), tt([0.5, 0.5, 0.5, 0.5]), 0),
 ])
 def test_iou(gt, pred, answer):
     print(bbox_iou(pred, gt).item(), answer)
+    # check_boxes([pred, gt])
     torch.testing.assert_allclose(bbox_iou(pred, gt).item(), answer)
