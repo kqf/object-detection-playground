067fd6f130cdb139f11dbed8a2c2106f4eb7ceed
kqf <q.dev.null@gmail.com>
2021-06-20 08:20:01 +0300

    Simplify back the loss (#275)
    * Don't use relu for the regression objective
    
    * Remove the powers from the loss function

--- detection/losses/v3.py
+++ detection/losses/v3.py
@@ -40,7 +40,7 @@ class CombinedLoss(torch.nn.Module):
 
         noobj = target[..., 0:1] != 1  # in paper this is Iobj_i
         nodet = self.noobjloss(
-            torch.relu(pred[objectness][noobj]) ** 0.5,
+            torch.relu(pred[objectness][noobj]),
             target[objectness][noobj]
         )
 
@@ -53,7 +53,7 @@ class CombinedLoss(torch.nn.Module):
         obj = target[..., 0] == 1  # in paper this is Iobj_i
         ious = bbox_iou(box_preds, target[bbox_all]).detach()
         det = self.regression(
-            torch.relu(pred[objectness][obj]),
+            pred[objectness][obj],
             target[objectness][obj] * ious[obj],
         )
 
