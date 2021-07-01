326c58ace28d2e322816e0703e90554d252b76cd
kqf <q.dev.null@gmail.com>
2021-07-01 07:37:51 +0200

    Fix dataset tests (#283)
    * Add the tests configuration
    
    * Fix the tests fixture
    
    * Fix the option naming

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,12 +49,13 @@ def size():
     return 256
 
 
+@pytest.fixture
 def n_samples(request):
-    return request.config.getoption("--num-images-per-batchs")
+    return request.config.getoption("--num-images-per-batch")
 
 
 @pytest.fixture
-def annotations(n_samples=n_samples):
+def annotations(n_samples):
     """
                                image_id          class_name  class_id rad_id   x_min   y_min   x_max   y_max
     0  50a418190bc3fb1ef1633bf9678929b3          No finding        14    R11     NaN     NaN     NaN     NaN
