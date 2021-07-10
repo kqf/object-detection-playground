9be4e2c0d5125a5d1954c8ebd568139e3f1d9a3b
kqf <q.dev.null@gmail.com>
2021-07-10 14:59:30 +0200

    Add nms tests (#290)
    * Rename the test case
    
    * Add the nms function
    
    * Fix the nms tests

---
+++ tests/test_nms.py
@@ -0,0 +1,23 @@
+import torch
+import pytest
+import numpy as np
+from detection.inference import nms
+from detection.plot import plot
+
+
+@pytest.fixture
+def candidates(n_candidates=13 + 26 + 52):
+    x = np.zeros((n_candidates, 6))
+    x[:, 0] = np.linspace(0, 0.6, n_candidates)
+    x[n_candidates // 2, 0] = 1
+    x[:, 1] = np.linspace(0.45, 0.55, n_candidates)
+    x[:, 2] = np.linspace(0.55, 0.45, n_candidates)
+    x[:, 3] = 0.2
+    x[:, 4] = 0.2
+    return torch.tensor(x)
+
+
+def test_nms(candidates):
+    img = torch.ones(3, 460, 460)
+    sup = nms(candidates).any(-1)
+    plot((img, candidates[:, 1:], sup), convert_bbox=True)
