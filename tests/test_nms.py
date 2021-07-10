67b8358e0dfb8818a081de67ff6741769d17f95c
Sasha <q.dev.null@gmail.com>
2021-07-10 15:02:19 +0200

    Use the mask after the nms

--- tests/test_nms.py
+++ tests/test_nms.py
@@ -20,4 +20,4 @@ def candidates(n_candidates=13 + 26 + 52):
 def test_nms(candidates):
     img = torch.ones(3, 460, 460)
     sup = nms(candidates)
-    plot((img, candidates[:, 1:], sup), convert_bbox=True)
+    plot((img, candidates[sup, 1:], []), convert_bbox=True)
