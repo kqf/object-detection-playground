76c72aac34c699cc3eaf66ca53e50f97dd59a9d5
Sasha <q.dev.null@gmail.com>
2021-07-10 14:58:22 +0200

    Fix the nms tests

--- tests/test_nms.py
+++ tests/test_nms.py
@@ -1,7 +1,7 @@
 import torch
 import pytest
 import numpy as np
-# from detection.ionference import nms
+from detection.inference import nms
 from detection.plot import plot
 
 
@@ -10,13 +10,14 @@ def candidates(n_candidates=13 + 26 + 52):
     x = np.zeros((n_candidates, 6))
     x[:, 0] = np.linspace(0, 0.6, n_candidates)
     x[n_candidates // 2, 0] = 1
-    x[:, 1] = np.linspace(0.2, 0.8, n_candidates)
-    x[:, 2] = np.linspace(0.8, 0.2, n_candidates)
+    x[:, 1] = np.linspace(0.45, 0.55, n_candidates)
+    x[:, 2] = np.linspace(0.55, 0.45, n_candidates)
     x[:, 3] = 0.2
     x[:, 4] = 0.2
-    return x
+    return torch.tensor(x)
 
 
 def test_nms(candidates):
     img = torch.ones(3, 460, 460)
-    plot((img, candidates[:, 1:], []), convert_bbox=True)
+    sup = nms(candidates).any(-1)
+    plot((img, candidates[:, 1:], sup), convert_bbox=True)
