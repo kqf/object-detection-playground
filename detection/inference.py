04a05fef25239efe368faa2052f6c05b9d757623
Sasha <q.dev.null@gmail.com>
2021-07-11 08:41:49 +0200

    Set the same parameters

--- detection/inference.py
+++ detection/inference.py
@@ -64,7 +64,7 @@ def iou(box1, box2):
     return bbox_iou(torch.tensor(box1[2:]), torch.tensor(box2[2:]))
 
 
-def non_max_suppression(bboxes, iou_threshold, threshold):
+def non_max_suppression(bboxes, iou_threshold=0.5, threshold=0.5):
     bboxes = [box for box in bboxes if box[1] > threshold]
     bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
     bboxes_after_nms = []
