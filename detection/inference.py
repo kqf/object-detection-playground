d8ad3ef0c0b5905ee622c4406a299a0356e0f3bf
kqf <q.dev.null@gmail.com>
2021-07-12 06:38:12 +0200

    Delete the old nms implementation (#295)
    * Set the same parameters
    
    * Remove the unused impolementation of NMS
    
    * Clean the performance check

--- detection/inference.py
+++ detection/inference.py
@@ -60,29 +60,6 @@ def merge_scales(predictions):
     return [torch.cat(x) for x in zip(*flat)]
 
 
-def iou(box1, box2):
-    return bbox_iou(torch.tensor(box1[2:]), torch.tensor(box2[2:]))
-
-
-def non_max_suppression(bboxes, iou_threshold, threshold):
-    bboxes = [box for box in bboxes if box[1] > threshold]
-    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
-    bboxes_after_nms = []
-
-    while bboxes:
-        chosen_box = bboxes.pop(0)
-
-        bboxes = [
-            box
-            for box in bboxes
-            if box[0] != chosen_box[0] or iou(chosen_box, box) < iou_threshold
-        ]
-
-        bboxes_after_nms.append(chosen_box)
-
-    return bboxes_after_nms
-
-
 def nms(bboxes, threshold=0.5, min_iou=0.5):
     # Filter out the boxes with low objectness score
     x = bboxes[bboxes[:, 0] > threshold]
