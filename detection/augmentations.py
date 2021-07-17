cb06633b32fad4b97a504c146f75abb115cc95b0
Sasha <q.dev.null@gmail.com>
2021-07-17 10:22:43 +0200

    Remove flips

--- detection/augmentations.py
+++ detection/augmentations.py
@@ -58,7 +58,7 @@ def transform(train=True, mean=None, std=None, size=32 * 13):
     train_transforms = []
     if train:
         train_transforms = [
-            alb.Flip(0.5)
+            # alb.Flip(0.5)
         ]
 
     return alb.Compose(train_transforms + transforms, bbox_params=bbox_params)
