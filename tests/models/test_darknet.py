c0fb5a123521b66964fd3fc38ff4a908d178168b
kqf <q.dev.null@gmail.com>
2021-07-17 10:31:07 +0200

    Remove legacy code (#308)
    * Remove the legacy module
    
    * Remove the legacy tests

--- tests/models/test_darknet.py
+++ tests/models/test_darknet.py
@@ -2,7 +2,6 @@ import pytest
 import torch
 
 from detection.models.darknet import Darknet
-from detection.models.legacy import build_model
 
 
 @pytest.fixture
@@ -23,15 +22,3 @@ def test_module(batch, size):
     assert l1.shape == (4, 1024, out_size, out_size)
     assert l2.shape == (4, 512, out_size * 2, out_size * 2)
     assert l3.shape == (4, 256, out_size * 4, out_size * 4)
-
-
-@pytest.mark.parametrize("size", [
-    32,
-    32 * 2,
-    32 * 10,
-])
-def test_legacy(batch, size):
-    model = torch.nn.Sequential(*build_model(3, 40))
-    l1 = model(batch)
-    out_size = size // 32
-    assert l1.shape == (4, 1024, out_size, out_size)
