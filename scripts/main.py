03ceaabe08de22367de7dc4bac1cf169f9c3304e
Sasha <q.dev.null@gmail.com>
2021-07-20 16:05:07 +0200

    Switch the modules

--- scripts/main.py
+++ scripts/main.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from click import Path as cpath
 
 from detection.data import read_data
-from detection.datasets.frcnn import DetectionDataset
 from detection.augmentations import transform
+from detection.datasets import DetectionDatasetV3
 from detection.model import build_model
 
 
@@ -18,7 +18,7 @@ def main(fin, logdir):
     # TODO: Fix me, overfit a single example
     df = df.loc[[3] * 60]
     print(df.head())
-    train = DetectionDataset(df, fin, transform(train=True))
+    train = DetectionDatasetV3(df, fin, transform(train=True))
 
     model = build_model()
     model.fit(train)
