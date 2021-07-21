0c6aaf7dbd662d12272de3342dff1598f446ab78
Sasha <q.dev.null@gmail.com>
2021-07-21 06:45:11 +0200

    Concatenate multiple blobs

--- detection/mc.py
+++ detection/mc.py
@@ -63,8 +63,13 @@ def annotations(n_points=32, h=2000, w=2000):
 def generate_to_directory(annotations, dirname, image_col="image_id"):
     path = Path(dirname)
     for image_id, blobs in annotations.groupby(image_col):
+
+        blobs = []
         for row in annotations.to_dict(orient="records"):
-            img = blob2image(make_blob(**row))
+            blobs.append(make_blob(**row))
+        blob = np.stack(blobs, axis=-1).any(-1)
+
+        img = blob2image(blob)
         ifile = f"{image_id}.png"
         cv2.imwrite(str(path / ifile), img)
     annotations.to_csv(path / "train.csv", index=False)
